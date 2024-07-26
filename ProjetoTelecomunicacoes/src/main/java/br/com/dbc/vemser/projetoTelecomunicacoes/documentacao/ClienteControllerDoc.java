bb4116a8a558eee31da37e4e3603326bf1054d13
DudaWendelMaia <dudawendelmaia@gmail.com>
2024-07-26 01:08:25 -0300

    Atualização da Documentação

--- ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/documentacao/ClienteControllerDoc.java
+++ ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/documentacao/ClienteControllerDoc.java
@@ -12,75 +12,72 @@ import javax.validation.Valid;
 import java.sql.SQLException;
 import java.util.List;
 
-
 public interface ClienteControllerDoc {
 
-    @Operation(summary = "Listar pessoas.", description = "Lista todas as pessoas do banco.")
+    @Operation(summary = "Listar clientes.", description = "Lista todos os clientes do banco de dados.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna uma lista de pessoas."),
+                    @ApiResponse(responseCode = "200", description = "Retorna uma lista de clientes."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
-    @GetMapping // GET localhost:8080/pessoa
+    @GetMapping // GET localhost:8080/cliente
     public ResponseEntity<List<ClienteDTO>> list();
 
-    @Operation(summary = "Listar pelo nome.", description = "Lista a pessoa com o respectivo nome passado.")
+    @Operation(summary = "Listar pelo nome.", description = "Lista os clientes com o respectivo nome passado.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna uma pessoa do banco."),
+                    @ApiResponse(responseCode = "200", description = "Retorna uma lista de clientes."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
-    @GetMapping("/byname") // GET localhost:8080/pessoa/byname?nome=Rafa
+    @GetMapping("/byname") // GET localhost:8080/cliente/byname?nome=Nome
     public ResponseEntity<List<ClienteDTO>> listByName(@RequestParam(value = "nome") String nome) throws SQLException;
 
-
-    @Operation(summary = "Listar pelo id.", description = "Lista a pessoa com o respectivo id passado.")
+    @Operation(summary = "Listar pelo id.", description = "Lista o cliente com o respectivo id passado.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna uma pessoa do banco."),
+                    @ApiResponse(responseCode = "200", description = "Retorna uma lista de clientes."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
-    @GetMapping("/{idCliente}") // GET localhost:8080/cliente/byname?nome=Rafa
+    @GetMapping("/{idCliente}") // GET localhost:8080/cliente/{idCliente}
     public ResponseEntity<List<ClienteDTO>> listById(@PathVariable("idCliente") Integer id) throws SQLException;
 
-
-    @Operation(summary = "Cria uma pessoa.", description = "Cria uma nova pessoa.")
+    @Operation(summary = "Criar um cliente.", description = "Cria um novo cliente.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna a pessoa criada."),
+                    @ApiResponse(responseCode = "200", description = "Retorna o cliente criado."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
-    @PostMapping // POST localhost:8080/pessoa
-    public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteCreateDTO pessoa) throws Exception;
+    @PostMapping // POST localhost:8080/cliente
+    public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteCreateDTO cliente) throws Exception;
 
-    @Operation(summary = "Atualizar uma pessoa.", description = "Atualiza uma pessoa pelo id dela.")
+    @Operation(summary = "Atualizar um cliente.", description = "Atualiza um cliente pelo id dele.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna a pessoa atualizada."),
+                    @ApiResponse(responseCode = "200", description = "Retorna o cliente atualizado."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
-    @PutMapping("/{idPessoa}") // PUT localhost:8080/pessoa/1000
-    public ResponseEntity<ClienteDTO> update(@PathVariable("idPessoa") Integer id, @Valid @RequestBody ClienteCreateDTO pessoaAtualizar) throws Exception;
+    @PutMapping("/{idCliente}") // PUT localhost:8080/cliente/{idCliente}
+    public ResponseEntity<ClienteDTO> update(@PathVariable("idCliente") Integer id, @Valid @RequestBody ClienteCreateDTO clienteAtualizar) throws Exception;
 
-    @Operation(summary = "Deletar uma pessoa.", description = "Deleta uma pessoa pelo id dela.")
+    @Operation(summary = "Deletar um cliente.", description = "Deleta um cliente pelo id dele.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna a pessoa deletada."),
+                    @ApiResponse(responseCode = "200", description = "Cliente deletado com sucesso."),
                     @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso."),
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção.")
             }
     )
-    @DeleteMapping("/{idPessoa}") // DELETE localhost:8080/pessoa/10
-    public ResponseEntity<Void> delete(@PathVariable("idPessoa") Integer id) throws Exception;
+    @DeleteMapping("/{idCliente}") // DELETE localhost:8080/cliente/{idCliente}
+    public ResponseEntity<Void> delete(@PathVariable("idCliente") Integer id) throws Exception;
 
 }
