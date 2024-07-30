8907283f921250291b0e7cd430f7ddd14276051b
HaralanS <haralansantana@gmail.com>
2024-07-30 17:39:16 -0300

    altera token service para usar jwt

--- ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/security/TokenService.java
+++ ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/security/TokenService.java
@@ -2,10 +2,15 @@ package br.com.dbc.vemser.projetoTelecomunicacoes.security;
 
 import br.com.dbc.vemser.projetoTelecomunicacoes.entity.UsuarioEntity;
 import br.com.dbc.vemser.projetoTelecomunicacoes.service.UsuarioService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Base64;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -13,11 +18,32 @@ import java.util.Optional;
 public class TokenService {
     static final String HEADER_STRING = "Authorization";
     private final UsuarioService usuarioService;
+    private static final String TOKEN_PREFIX = "BEARER ";
 
-    public String getToken(UsuarioEntity usuarioEntity) {
-        String tokenTexto = usuarioEntity.getLogin() + ";" + usuarioEntity.getSenha(); // thiago.gomes;1234 > 923y978126391
-        String token = Base64.getEncoder().encodeToString(tokenTexto.getBytes());
-        return token;
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+//    public String getToken(UsuarioEntity usuarioEntity) {
+//        String tokenTexto = usuarioEntity.getLogin() + ";" + usuarioEntity.getSenha(); // thiago.gomes;1234 > 923y978126391
+//        String token = Base64.getEncoder().encodeToString(tokenTexto.getBytes());
+//        return token;
+//    }
+
+    public String gerarTokenJWT(UsuarioEntity usuarioEntity) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + Long.parseLong(expiration));
+
+        return TOKEN_PREFIX +
+                Jwts.builder()
+                        .setIssuer("vemser-api")
+                        .claim(Claims.ID, usuarioEntity.getIdUsuario().toString())
+                        .setIssuedAt(now)
+                        .setExpiration(exp)
+                        .signWith(SignatureAlgorithm.HS256, secret)
+                        .compact();
     }
 
     public Optional<UsuarioEntity> isValid(String token) {
