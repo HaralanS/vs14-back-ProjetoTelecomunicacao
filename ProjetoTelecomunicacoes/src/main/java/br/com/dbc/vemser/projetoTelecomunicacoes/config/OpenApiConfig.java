b8dc2a4b2a0fce6c9ed0a3bfe51686ff8994c1d7
HaralanS <haralansantana@gmail.com>
2024-07-29 21:19:47 -0300

    adiciona as alteracoes necessarias no banco de dados atraves do migrations e implementa o spring security no projeto

--- ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/config/OpenApiConfig.java
+++ ProjetoTelecomunicacoes/src/main/java/br/com/dbc/vemser/projetoTelecomunicacoes/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package br.com.dbc.vemser.projetoTelecomunicacoes.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -7,6 +9,12 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @Configuration
 public class OpenApiConfig {
 
